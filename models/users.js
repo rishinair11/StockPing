d3bdf3940ba6e18c9d890c1697bc8651c99b0f5c
Rishikesh Nair <nrish123@in.ibm.com>
2018-08-30 16:40:39 +0530

    Login functionality in API.

--- models/users.js
+++ models/users.js
@@ -16,6 +16,7 @@ const UserSchema = mongoose.Schema({
     },
     watchlist: {
         type: Array,
+        default: [],
         required: false
     }
 });
--- routes/route.js
+++ routes/route.js
@@ -6,78 +6,95 @@ const bcrypt = require('bcrypt');
 const User = require('../models/users');
 
 //get all users
-router.get('/getAll', function (req, res, next) {
-    User.find(function (err, users) {
-        res.json(users);
-    })
+router.get('/getAll', function(req, res, next) {
+  User.find({}, 'username watchlist emailID', function(err, users) {
+    if (err) {
+      throw err
+    } else {
+      res.json(users);
+    }
+  });
 });
 
-router.get('/getOne', function (req, res, next) {
-    User.findOne({
-        username: req.query.username
-    }, function (err, result) {
-        if (err) {
-            throw err
-        } else {
-            res.json(result.watchlist);
-            console.log(result.watchlist);
-        }
-    });
+router.post('/login', function(req, res, next) {
+  User.find({
+    username: req.body.username,
+    password: req.body.password
+  }, 'username watchlist', function(err, users) {
+    if (err) {
+      throw err
+    } else {
+      res.json(users);
+    }
+  });
+});
+
+router.get('/getOne', function(req, res, next) {
+  User.findOne({
+    username: req.query.username
+  }, function(err, result) {
+    if (err) {
+      throw err
+    } else {
+      res.json(result.watchlist);
+      console.log(result.watchlist);
+    }
+  });
 });
 
 //add user
-router.post('/register', function (req, res, next) {
-    console.log(req.body);
-    let newUser = new User({
-        username: req.body.username,
-        password: bcrypt.hashSync(req.body.password, 10), //added hashed password support in backend (for testing purposes only)
-        emailID: req.body.emailID,
-        watchlist: null
-    });
+router.post('/register', function(req, res, next) {
+  console.log(req.body);
+  let newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+    emailID: req.body.emailID,
+    watchlist: null
+  });
 
-    newUser.save(function (err, user) {
-        if (err) {
-            res.json({
-                msg: 'Error adding user!',
-                error: err
-            });
-        } else {
-            res.json({
-                msg: 'User added!' + user
-            });
-        }
-    });
+  newUser.save(function(err, user) {
+    if (err) {
+      res.json({
+        msg: 'Error adding user!',
+        error: err
+      });
+    } else {
+      res.json({
+        msg: 'User added!' + user
+      });
+    }
+  });
 
 });
 
-router.post('/update', function (req, res, next) {
-    User.findOneAndUpdate({
-        username: req.body.username
-    }, {
-        watchlist: req.body.watchlist
-    }, function (err, result) {
-        if (err) {
-            throw err
-        } else {
-            res.json({
-                msg: "User updated"
-            });
-        }
-    });
+router.post('/update', function(req, res, next) {
+  User.findOneAndUpdate({
+    username: req.body.username
+  }, {
+    watchlist: req.body.watchlist
+  }, function(err, result) {
+    if (err) {
+      throw err
+    } else {
+      res.json({
+        msg: "User updated"
+      });
+    }
+  });
 });
 
 
 //delete user
-router.delete('/remove', function (req, res, next) {
-    User.remove({
-        _id: req.params.id
-    }, function (err, result) {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    });
+router.delete('/remove', function(req, res, next) {
+  User.remove({
+    _id: req.params.id
+  }, function(err, result) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 module.exports = router;
